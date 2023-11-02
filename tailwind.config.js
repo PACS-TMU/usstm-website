0354a97554a457b44582ece7588a0c5f54a11dd7
williamKhine <williamkhine@outlook.com>
2023-11-02 14:15:11 -0400

    Config Tailwind to work in components dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./components/*.{html,js}"],
   theme: {
     extend: {},
   },
