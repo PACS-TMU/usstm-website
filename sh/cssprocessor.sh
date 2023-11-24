2c957b9987d10c7940d846c8b0551c5edb1d0848
williamKhine <williamkhine@outlook.com>
2023-11-24 17:56:37 -0500

    simplified

--- sh/cssprocessor.sh
+++ sh/cssprocessor.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # while true; do
-    rm ./css/in/main.css
-    cat ./css/style.css >> ./css/in/main.css
-    echo "" >> ./css/in/main.css
-    for file in ./css/*.css; do
-        if [ $(basename $file) != "style.css" ]; then
-            cat $file >> ./css/in/main.css
-            echo "" >> ./css/in/main.css
+    cat ./css/global.css > ./css/in/main-in.css
+    echo "" >> ./css/in/main-in.css
+    # for file in ./css/*.css; do
+    for file in $(find ./css -name '*.css'); do
+        if [ $(basename $file) != "global.css" ] || 
+        [ $(basename $file) != "main*.css" ]; then
+            cat $file >> ./css/in/main-in.css
+            echo "" >> ./css/in/main-in.css
         fi
     done
-    npx tailwindcss -i ./css/in/main.css -o ./css/out/main.css
+    npx tailwindcss -i ./css/in/main-in.css -o ./css/out/main-out.css
     exit 0
     # sleep 2
 # done
