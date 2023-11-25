4a12f01a78adc96adaf5379d87b69f754029f93b
williamKhine <williamkhine@outlook.com>
2023-11-24 20:14:44 -0500

    Update tailwind.config.js
    Co-authored-by: Hamid Reza Khoramrokh <HamidRezaKhoram@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}", "./components/*.{html,js}"],
   theme: {
-    colors: {
-      "background": "#FFFFFF",
-      "foreground": "#161616",
-      "highlight": "#D67940",
-      "highlight-dark": "#001D56"
+    extend: {
+      colors: {
+        "background": "#FFFFFF",
+        "foreground": "#161616",
+        "highlight": "#D67940",
+        "highlight-dark": "#001D56",
+      },
+      fontFamily: {
+        "sans": ["Abril"],
+        "serif": ["Lato"],
+      },
     },
-    fontFamily: {
-      "sans": ["Abril"],
-      "serif": ["Lato"],
-    },
-    extend: {},
   },
   plugins: [],
 }
