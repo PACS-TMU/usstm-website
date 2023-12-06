c819858855ada9ab0c445b9c0cafabc80176295f
monakouzi <mo.nakouzi03@gmail.com>
2023-12-05 19:57:55 -0500

    Make pillars into flipping cards

--- src/app/_components/homepage/pillars.jsx
+++ src/app/_components/homepage/pillars.jsx
@@ -1,3 +1,5 @@
+import "./pillars.css";
+
 export default function Pillars() {
   return (
     <>
@@ -9,17 +11,47 @@ export default function Pillars() {
                         <h1 className="text-6xl lg:text-8xl text-background lg:px-8 lg:mx-8 text-center">Our Four Pillars</h1>
                     </div>
                     <div className="pillar-cirlces h-full relative py-6 gap-6 lg:gap-y-20 grid grid-cols-1 grid-rows-4 lg:grid-cols-2 lg:grid-rows-5 max-h-[100%]">
-                        <div className="pillar1 mr-[40%] rounded-full w-[50%] lg:mr-0 lg:w-[80%] aspect-square row-start-1 col-start-1 lg:row-span-2 self-center justify-self-center flex items-center justify-center bg-highlight">
-                            <h2 className="text-background text-xl lg:text-2xl text-center">CONSISTENCY</h2>
+                        <div className="pillar1 cardBox perspective rounded-full mr-[40%] w-[50%] lg:mr-0 lg:w-[80%] xl:w-[90%] row-start-1 col-start-1 lg:row-span-2 self-center justify-self-center flex items-center justify-center">
+                            <div className="card w-full h-full rounded-full aspect-square bg-highlight">
+                                <div className="cardFront h-full flex justify-center items-center text-background text-md lg:text-lg xl:text-2xl text-center font-sans">
+                                    CONSISTENCY
+                                </div>
+                                <div className="cardBack h-full flex justify-center items-center self-center text-background text-sm lg:text-md text-center">
+                                USSTM is committed to ensuring consistency. Whether that is throughout our events or  the planning process, we ensure to be 
+                                consistent and transparent with all students in the Faculty of Science.
+                                </div>
+                            </div>
                         </div>
-                        <div className="pillar2 ml-[40%] rounded-full w-[50%] lg:ml-0 lg:w-[80%] aspect-square row-start-2 lg:col-start-2 lg:row-span-2 lg:row-end-3 self-center justify-self-center flex items-center justify-center bg-highlight">
-                            <h2 className="text-background text-xl lg:text-2xl text-center">TRANSPARENCY</h2>
+                        <div className="pillar2 cardBox perspective rounded-full ml-[40%] w-[50%] lg:ml-0 lg:w-[80%] xl:w-[85%] row-start-2 lg:col-start-2 lg:row-span-2 lg:row-end-3 self-center justify-self-center flex items-center justify-center">
+                            <div className="card w-full h-full rounded-full aspect-square bg-highlight">
+                                <div className="cardFront h-full flex justify-center items-center text-background text-md lg:text-lg xl:text-2xl text-center font-sans">
+                                    TRANSPARENCY
+                                </div>
+                                <div className="cardBack h-full flex justify-center items-center self-center text-background text-sm lg:text-md text-center">
+                                    Description.
+                                </div>
+                            </div>
                         </div>
-                        <div className="pillar3 mr-[40%] rounded-full w-[50%] lg:mr-0 lg:w-[80%] aspect-square row-start-3 col-start-1 lg:row-span-2 lg:row-end-4 self-center justify-self-center flex items-center justify-center bg-highlight">
-                            <h2 className="text-background text-xl lg:text-2xl text-center">SUPPORT</h2>
+                        <div className="pillar3 cardBox perspective rounded-full mr-[40%] w-[50%] lg:mr-0 lg:w-[80%] xl:w-[90%] row-start-3 col-start-1 lg:row-span-2 lg:row-end-4 self-center justify-self-center flex items-center justify-center">
+                            <div className="card w-full h-full rounded-full aspect-square bg-highlight">
+                                <div className="cardFront h-full flex justify-center items-center text-background text-md lg:text-lg xl:text-2xl text-center font-sans">
+                                    SUPPORT
+                                </div>
+                                <div className="cardBack h-full flex justify-center items-center self-center text-background text-sm lg:text-md text-center">
+                                USSTM is commited to providing support and activities, events, and resources that will enhance the Membership's professional 
+                                experience and marketable skills.
+                                </div>
+                            </div>
                         </div>
-                        <div className="pillar4 ml-[40%] rounded-full w-[50%] lg:ml-0 lg:w-[80%] aspect-square row-start-4 lg:col-start-2 lg:row-span-2 lg:row-end-5 self-center justify-self-center flex items-center justify-center bg-highlight">
-                            <h2 className="text-background text-xl lg:text-2xl text-center">UNITY</h2>
+                        <div className="pillar4 cardBox perspective rounded-full ml-[40%] w-[50%] lg:ml-0 lg:w-[80%] xl:w-[90%] row-start-4 lg:col-start-2 lg:row-span-2 lg:row-end-5 self-center justify-self-center flex items-center justify-center">
+                            <div className="card w-full h-full rounded-full aspect-square bg-highlight">
+                                <div className="cardFront h-full flex justify-center items-center text-background text-md lg:text-lg xl:text-2xl text-center font-sans">
+                                    UNITY
+                                </div>
+                                <div className="cardBack h-full flex justify-center items-center self-center text-background text-sm lg:text-md text-center">
+                                    Description.
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
