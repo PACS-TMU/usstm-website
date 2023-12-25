a4105911676e64c8548f5456340b5e4a5a9c50c1
Andre <dtandre331@gmail.com>
2023-12-24 23:11:07 -0500

    Update page and data #37

--- src/app/about/past-boards/page.jsx
+++ src/app/about/past-boards/page.jsx
@@ -19,43 +19,38 @@ export default function PastBoards() {
     console.log(boardItems);
 
     return (
-        <>
-            <div className="p-4">
-                <h1 className="text-2xl font-bold mb-4">Past Boards</h1>
-                
-                {boardItems.map((boardItem) => (
-                    <div key={boardItem.id} className="mb-8">
-                        <h2 className="text-xl font-bold mb-2">{boardItem.year}</h2>
-                        <div className="flex flex-col space-y-4">
-                            {boardItem.members.map((id) => (
-                                id.people.map((member) => (
-                                    <div key={member.id} className="bg-gray-100 p-4 rounded-md">
-                                        <p className="text-sm font-semibold mb-2">Position: {member.position}</p>
-                                        {
-                                            (() => {
-                                                if (Array.isArray(member.name)) {
-                                                    return (
-                                                        <div>
-                                                            <p className="text-sm font-semibold mb-2">Names:</p>
-                                                            <ul className="list-disc pl-4">
-                                                                {member.name.map((name, index) => (
-                                                                    <li key={index} className="text-sm">{name}</li>
-                                                                ))}
-                                                            </ul>
-                                                        </div>
-                                                    );
-                                                } else {
-                                                    return <p className="text-sm font-semibold mb-2">Name: {member.name}</p>;
-                                                }
-                                            })()
-                                        }
-                                    </div>
-                                ))
-                            ))}
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </>
+        <div className="p-4 mx-auto max-w-screen-xl">
+            <h1 className="text-5xl font-bold mb-4 text-center">Past Boards</h1>
+
+            {boardItems.map((boardItem) => (
+                <div key={boardItem.id} className="mb-4">
+                    <h2 className="text-xl font-bold mb-2 text-center">{boardItem.year}</h2>
+
+                    {boardItem.members.map((section) => {
+                        const ID = section.id.replace(/-22$/, '');
+
+                        return (
+                            <div key={ID} className="mb-4 text-center">
+                                <h3 className="text-lg font-semibold mb-2">{ID}</h3>
+
+                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-auto justify-center">
+                                    {section.people.map((member) => {
+                                        const POS = member.position;
+                                        const NAME = Array.isArray(member.name) ? member.name.join(', ') : member.name;
+
+                                        return (
+                                            <div key={member.id} className="bg-gray-100 p-4 rounded-md">
+                                                <p className="text-sm font-semibold mb-2">{POS}</p>
+                                                <p className="text-sm font-light mb-2">{NAME}</p>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            ))}
+        </div>
     );
 }
