f35309ef189e16d7e3b9aa911053bca3539c4429
Viri Nguyen <grcng@viris-mbp.home>
2023-12-26 18:28:58 -0500

    Database for Annual Events

--- src/app/events/annual/page.jsx
+++ src/app/events/annual/page.jsx
@@ -1,7 +1,30 @@
+"use client";
+import AnnualEvent from '@/app/_components/events/annual-event';
+import { useEffect, useState } from 'react';
+
 export default function Annual() {
+    const [annualEvents, setAnnualEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/data/annual-events.json');
+                const data = await response.json();
+                setAnnualEvents(data);
+            } catch (error) {
+                console.error('Mo is too fat to fetch data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            <h1>Annual</h1>
-        </div>
+        <section id='AnnualEvents' className='main'>
+            <p className='text-center'>Our Annual Events</p>
+            {annualEvents.map((annualEvent) => (
+                <AnnualEvent key={annualEvent.id} event={annualEvent} />
+            ))}
+        </section>
     )
 }
