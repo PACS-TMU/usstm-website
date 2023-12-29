d9ab0df43e0602f09e170023571a9a0e2471e410
monakouzi <mo.nakouzi03@gmail.com>
2023-12-29 16:20:11 -0500

    styling changes

--- src/app/community/groups-unions/page.jsx
+++ src/app/community/groups-unions/page.jsx
@@ -9,28 +9,35 @@ export default function GroupsUnions() {
         navigator.clipboard.writeText(email);
         setCopiedEmail(email);
     };
-    
+
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', gap: '20px' }}>
-            <h1 style={{ fontSize: '2em', textAlign: 'center' }}>Groups And Unions</h1>
-        {sgcuData.map((group, index) => (
-        <div key={index} style={{ textAlign: 'center', padding: '10px', border: '1px solid #ccc' }}>
-          <img
-            src={`/images/sg-logos/${group['group-logo']}`}
-            alt={group['group-name']}
-            style={{ width: '100px', height: '100px', marginBottom: '10px', cursor: 'pointer' }}
-            onClick={() => copyToClipboard(group['contact-info'][1])}
-          />
-          <h2
-            style={{ margin: '0', cursor: 'pointer' }}
-            onClick={() => copyToClipboard(group['contact-info'][1])}
-          >
-            {group['group-name']}
-          </h2>
-          {copiedEmail === group['contact-info'][1] && <p style={{ color: 'green' }}>Email Copied!</p>}
+        <div className='main p-4'>
+            <h1 className="text-5xl font-semibold pb-4 leading-relaxed" style={{ textAlign: 'center' }}>Groups And Unions</h1>
+            <div className='grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-4 items-center justify-center' style={{ display: 'grid', gap: '20px' }}>
+                {sgcuData.map((group, index) => (
+                    <div
+                        key={index}
+                        style={{ textAlign: 'center', padding: '10px' }}
+                        className='flex flex-col aspect-square items-center justify-center bg-gray-100 shadow-md rounded-md'
+                        onClick={() => copyToClipboard(group['contact-info'][1])}
+                    >
+                        <img
+                            src={`/images/sg-logos/${group['group-logo']}`}
+                            alt={group['group-name']}
+                            className='mx-auto'
+                            style={{ width: '100px', height: 'auto', marginBottom: '10px', cursor: 'pointer' }}
+                            
+                        />
+                        <p
+                            style={{ margin: '0', cursor: 'pointer' }}
+                            className='font-semibold'
+                        >
+                            {group['group-name']}
+                        </p>
+                        {copiedEmail === group['contact-info'][1] && <p style={{ color: 'green' }}>Email Copied!</p>}
+                    </div>
+                ))}
+            </div>
         </div>
-        ))}
-        </div>
-            
     )
 }
