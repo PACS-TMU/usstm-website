3893617ddb4ae3c380da73c515cc0092e120d20e
monakouzi <mo.nakouzi03@gmail.com>
2024-01-03 23:40:41 -0500

    slider and responsiveness changes, changed all class to classNames, removed onclick attributes

--- src/app/about/your-team/page.jsx
+++ src/app/about/your-team/page.jsx
@@ -2,96 +2,92 @@
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-function Directors() {
-    const settings = {
-
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-     
-    };
-
-    return (
-        <Slider {...settings}>
-        <img
-      src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-      style={{
-        display: 'block',
-        height: '100%',
-        margin: 'auto',
-        width: '100%'
-      }}
-    />
-        <img
-      src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-      style={{
-        display: 'block',
-        height: '100%',
-        margin: 'auto',
-        width: '100%'
-      }}
-    />
-        </Slider>
-    );
-}
+import './your-team.css';
 
 export default function YourTeam() {
+    function Directors() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
 
+        return (
+            <Slider {...settings}>
+                <img
+                    src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    style={{
+                        display: 'flex',
+                        height: '100%',
+                        margin: 'auto',
+                        width: '100%'
+                    }}
+                />
+                <img
+                    src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    style={{
+                        display: 'flex',
+                        height: '100%',
+                        margin: 'auto',
+                        width: '100%'
+                    }}
+                />
+            </Slider>
+        );
+    }
 
-    
     return (
-        <div>
-            
+        <div className="main flex flex-col">
+
             {/* Header for page*/}
             <header className="text-4xl lg:text-5xl text-center bg-highlight-dark p-10">
-            
-                <h1 class="text-white">
+                <h1 className="text-white">
                     Your 2023-2024 Team
                 </h1>
-               
             </header>
 
-            <div className="flex flex-col space-y-3 px-96">
+            <div className="flex flex-col space-y-3 w-[90%] self-center">
                 {/* This div element is to encase all the photos and labels*/}
 
-               
+
                 <div className="space-y-5">
                     <h1 className="text-center pt-5 text-3xl text-highlight-dark">
                         BOARD OF DIRECTORS
                     </h1>
 
-                     <Directors/>
+                    <Directors className="hidden" />
 
                     <p className=" pt-2">
-                        The USSTM Board of Directors is a dynamic group of student leaders at 
+                        The USSTM Board of Directors is a dynamic group of student leaders at
                         Toronto Metropolitan University, dedicated to enhancing the academic and s
-                        ocial experience for the Faculty of Science. The Board is committed to 
-                        fostering a vibrant and inclusive community. Elected annually, these student 
-                        leaders embody the spirit of innovation and collaboration, working to create a 
-                        fulfilling experience for science students at TMU. 
+                        ocial experience for the Faculty of Science. The Board is committed to
+                        fostering a vibrant and inclusive community. Elected annually, these student
+                        leaders embody the spirit of innovation and collaboration, working to create a
+                        fulfilling experience for science students at TMU.
                     </p>
 
-                    <a href="/about/your-team/directors"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
-                    
+                    <a href="/about/your-team/directors">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
+
                 </div>
 
 
                 <div className="space-y-5 pt-12">
                     <h1 className="text-center pt-5 text-3xl text-highlight-dark">
-                    PRESIDENT'S COMMITTEE
+                        PRESIDENT'S COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         The President's Committee is responsible for assiting and aiding the President in thier tasks
                     </p>
 
-                    <a href="/about/your-team/president-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                    <a href="/about/your-team/president-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
 
                 </div>
 
@@ -101,16 +97,17 @@ export default function YourTeam() {
                         OPERATIONS COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Operations goes here 
-                        Text about Operations goes here 
-                        Text about Operations goes here 
-                        Text about Operations goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
+                        Text about Operations goes here
+                        Text about Operations goes here
+                        Text about Operations goes here
+                        Text about Operations goes here
                         Text about Operations goes here
                     </p>
-                    <a href="/about/your-team/operations-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                    <a href="/about/your-team/operations-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -118,16 +115,17 @@ export default function YourTeam() {
                         FROSH COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Frosh Committee goes here 
-                        Text about Frosh Committee goes here 
-                        Text about Frosh Committee goes here 
-                        Text about Frosh Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Frosh Committee goes here
-                    </p> 
-                    <a href="/about/your-team/frosh-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Frosh Committee goes here
+                        Text about Frosh Committee goes here
+                        Text about Frosh Committee goes here
+                        Text about Frosh Committee goes here
+                    </p>
+                    <a href="/about/your-team/frosh-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -135,16 +133,17 @@ export default function YourTeam() {
                         ACADEMICS COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Academics Committee goes here 
-                        Text about Academics Committee goes here 
-                        Text about Academics Committee goes here 
-                        Text about Academics Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Academics Committee goes here
-                    </p> 
-                    <a href="/about/your-team/academics-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Academics Committee goes here
+                        Text about Academics Committee goes here
+                        Text about Academics Committee goes here
+                        Text about Academics Committee goes here
+                    </p>
+                    <a href="/about/your-team/academics-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -152,16 +151,17 @@ export default function YourTeam() {
                         FORMAL COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Formal Committee goes here 
-                        Text about Formal Committee goes here 
-                        Text about Formal Committee goes here 
-                        Text about Foraml Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Formal Committee goes here
-                    </p>  
-                    <a href="/about/your-team/formal-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Formal Committee goes here
+                        Text about Formal Committee goes here
+                        Text about Foraml Committee goes here
+                        Text about Formal Committee goes here
+                    </p>
+                    <a href="/about/your-team/formal-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -169,16 +169,17 @@ export default function YourTeam() {
                         FINANCE COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Finance Committee goes here 
-                        Text about Finance Committee goes here 
-                        Text about Finance Committee goes here 
-                        Text about Finance Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Finance Committee goes here
-                    </p> 
-                    <a href="/about/your-team/finance-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Finance Committee goes here
+                        Text about Finance Committee goes here
+                        Text about Finance Committee goes here
+                        Text about Finance Committee goes here
+                    </p>
+                    <a href="/about/your-team/finance-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -186,16 +187,17 @@ export default function YourTeam() {
                         EXTERNAL COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Exterbal  Committee goes here 
-                        Text about External Committee goes here 
-                        Text about External Committee goes here 
-                        Text about External Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
+                        Text about Exterbal  Committee goes here
                         Text about External Committee goes here
-                    </p> 
-                    <a href="/about/your-team/extermal-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about External Committee goes here
+                        Text about External Committee goes here
+                        Text about External Committee goes here
+                    </p>
+                    <a href="/about/your-team/extermal-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -203,16 +205,17 @@ export default function YourTeam() {
                         EVENTS COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Events Committee goes here 
-                        Text about Events Committee goes here 
-                        Text about Events Committee goes here 
-                        Text about Events Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Events Committee goes here
-                    </p> 
-                    <a href="/about/your-team/events-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Events Committee goes here
+                        Text about Events Committee goes here
+                        Text about Events Committee goes here
+                        Text about Events Committee goes here
+                    </p>
+                    <a href="/about/your-team/events-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -220,16 +223,17 @@ export default function YourTeam() {
                         EQUITY COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Equity Committee goes here 
-                        Text about Equity Committee goes here 
-                        Text about Equity Committee goes here 
-                        Text about Equity Committee goes here 
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
                         Text about Equity Committee goes here
-                    </p> 
-                    <a href="/about/your-team/equity-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                        Text about Equity Committee goes here
+                        Text about Equity Committee goes here
+                        Text about Equity Committee goes here
+                        Text about Equity Committee goes here
+                    </p>
+                    <a href="/about/your-team/equity-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
 
                 <div className="space-y-5 pt-12">
@@ -237,25 +241,17 @@ export default function YourTeam() {
                         COMMUNICATIONS COMMITTEE
                     </h1>
 
-                     <Directors/>
-                     <p className=" pt-2">
-                        Text about Communicaitons Committee goes here 
-                        Bro I dont know why I wrote out every single one of these 'p' tags 
-                        This is so time consuming I swear. 
-                    </p> 
-                    <a href="/about/your-team/comms-committee"> 
-                    <button class="teamButton py-3" onclick="YourTeam()"> Meet the Team</button></a>
+                    <Directors className="hidden" />
+                    <p className=" pt-2">
+                        Text about Communicaitons Committee goes here
+                        Bro I dont know why I wrote out every single one of these 'p' tags
+                        This is so time consuming I swear.
+                    </p>
+                    <a href="/about/your-team/comms-committee">
+                        <button className="teamButton py-3"> Meet the Team</button>
+                    </a>
                 </div>
-
-
-                
-
-
-
-                
             </div>
-
-            
         </div>
     )
 }
