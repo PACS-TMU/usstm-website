f67bdc47242efeb6254784c8ff00ce41747e511d
Zil <zil.pavdigh@gmail.com>
2024-01-10 23:32:49 -0500

    most ticket tasks

--- src/app/community/groups-unions/page.jsx
+++ src/app/community/groups-unions/page.jsx
@@ -5,6 +5,11 @@ import Image from 'next/image';
 
 export default function GroupsUnions() {
     const [copiedEmail, setCopiedEmail] = useState(null);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const studentGroups = sgcuData.filter(group => group.type === 'SG');
+    const courseUnions = sgcuData.filter(group => group.type === 'CU');
+    const combinedGroups = [...studentGroups, ...courseUnions];
 
     const copyToClipboard = (email) => {
         navigator.clipboard.writeText(email);
@@ -19,22 +24,31 @@ export default function GroupsUnions() {
         window.open(instagramUrl, '_blank');
     };
 
+    
+
     return (
         <div className='main p-4'>
             <h1 className="text-5xl font-semibold pb-4 leading-relaxed" style={{ textAlign: 'center' }}>Groups And Unions</h1>
             <div className='grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-4 items-center justify-center' style={{ display: 'grid', gap: '20px' }}>
-                {sgcuData.map((group, index) => (
+                {combinedGroups.map((group, index) => (
                     <div
                         key={index}
-                        style={{ textAlign: 'center', padding: '10px' }}
+                        style={{ textAlign: 'center', padding: '10px', display: 'grid', gridTemplateRows: '50% 50%' }}
                         className='flex flex-col aspect-square items-center justify-center bg-gray-100 shadow-md rounded-md'
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
                     >
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                         <Image
                             src={`/images/sg-logos/${group['group-logo']}`}
                             alt={group['group-name']}
+                            width={100}
+                            height={100}
                             className='mx-auto'
-                            style={{ width: '100px', height: 'auto', marginBottom: '10px', cursor: 'pointer' }}
+                            style={{marginBottom: '10px', cursor: 'pointer' }}
                         />
+                    </div>
+                    <div style={{ textAlign: 'center' }}>    
                         <p
                             style={{ margin: '0', cursor: 'pointer' }}
                             className='font-semibold'
@@ -44,20 +58,28 @@ export default function GroupsUnions() {
                         {copiedEmail === group['contact-info'][1] && <p style={{ color: 'green' }}>Email Copied!</p>}
                         
                         <div className="flex mt-2">
-                            <button
-                                className="bg-blue-500 text-white px-4 py-2 mr-2"
-                                onClick={() => copyToClipboard(group['contact-info'][1])}
-                            >
-                                Copy Email
-                            </button>
-                            <button
-                                className="bg-purple-500 text-white px-4 py-2"
-                                onClick={() => redirectToInstagram(group['instagram-url'])}
-                            >
-                                Instagram
-                            </button>
+                            {hoveredIndex === index && (
+                                <>
+                                    {group['contact-info'][1] && (
+                                    <button
+                                    className="rounded-full bg-blue-500 text-white px-4 py-2 mr-2 flex items-center"
+                                    onClick={() => copyToClipboard(group['contact-info'][1])}
+                                    >
+                                        Copy Email
+                                    </button>)}
+                                    {group ['contact-info'][0].match(/href='(https:\/\/.*?)'/) && (
+                                        <button
+                                        className="rounded-full bg-purple-500 text-white px-4 py-2 flex items-center"
+                                        onClick={() => redirectToInstagram(group['contact-info'][0].match(/href='(https:\/\/.*?)'/)[1])}
+                                        >
+                                            Instagram
+                                        </button>
+                                    )}
+                                </>
+                            )}
                         </div>
-                    </div>
+                     </div>
+                  </div>
                 ))}
             </div>
         </div>
