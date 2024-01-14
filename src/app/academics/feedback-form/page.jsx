54144cb9378e19086065acf7105141fe2dc229e3
monakouzi <mo.nakouzi03@gmail.com>
2024-01-14 17:53:17 -0500

    adjusting sizes

--- src/app/academics/feedback-form/page.jsx
+++ src/app/academics/feedback-form/page.jsx
@@ -1,28 +1,32 @@
+"use client";
 import Header from "@/app/_components/general/header";
+import { useEffect } from "react";
+
+
 
 export default function Feedback() {
     return (
         <section>
             <Header title={`Academic Feedback Form`} />
             <div className="main">
-                <p className="lg:text-lg pb-4 text-left max-w-7xl mx-auto">
+                <p className="lg:text-lg pb-4 text-left w-full max-w-7xl mx-auto">
                     This form is for the USSTM to gather information and experiences from students who believe their course or professor was unfair, unhelpful or 
                     if someone was mistreated by an instructor in a course.
                 </p>
-                <p className="lg:text-lg pb-4 text-left max-w-7xl mx-auto">
+                <p className="lg:text-lg pb-4 text-left w-full max-w-7xl mx-auto">
                     Please let us keep this professional and constructive, the purpose is to give ANONYMOUS 
                     feedback to the professor and the faculty so we can work together to improve our learning environment! Filling out this form does not mean that 
                     any issues will be solved, however we promise that we will do our best to address them.
                 </p>
-                <p className="lg:text-lg pb-4 text-left max-w-7xl mx-auto">
+                <p className="lg:text-lg pb-4 text-left w-full max-w-7xl mx-auto">
                     If you have any questions or concerns, please reach out to our VP Academic 
                     at <a href="mailto:vp.academic@usstm.ca" target="_blank">vp.academic@usstm.ca</a>.
                 </p>
                 <iframe
                     src="https://docs.google.com/forms/d/e/1FAIpQLSfoAHIv_COKTku-omHjU7jCdPrShy9gxLLBMiF3eqkGFvnuTg/viewform?embedded=true"
                     width="640"
-                    height="1720"
-                    className="mx-auto w-full"
+                    height="1745"
+                    className="mx-auto w-full max-w-7xl"
                 >
                     Loading…
                 </iframe>
