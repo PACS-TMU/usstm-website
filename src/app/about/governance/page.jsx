c5c38f17a0bbd0d73c7d154ba7dbef52081f0b08
bensonzhang04 <benson.zhang@torontomu.ca>
2024-01-16 23:48:38 -0500

    added a div for governing docs

--- src/app/about/governance/page.jsx
+++ src/app/about/governance/page.jsx
@@ -4,83 +4,112 @@ import Directors from "@/app/about/governance/components/directors";
 import Membership from "@/app/about/governance/components/membership";
 import Meeting from "@/app/about/governance/components/meeting";
 import { useState } from "react";
+import { GrDocumentText } from "react-icons/gr";
 
 export default function Governance() {
     const [currentTab, setCurrentTab] = useState('directors');
 
     return (
         <section className="governance-page lg:text-lg">
-
             <Header title="Governance" />
-            <div id="content" className="main">
-                <div id="introduction" className="px-6 pb-5">
-                    <p className="block py-2">
-                        Our commitment to transparency and excellence is reflected in the way we govern our society. Our elected body of students from the
-                        Faculty of Science is dedicated to serving and representing you, the students. Learn more about the structure and leadership
-                        that drives our mission forward.
-                    </p>
-                </div>
-                <div className="content px-6">
-                    <div className="space-y-2 flex flex-col lg:flex-row lg:space-y-0 text-highlight-dark font-semibold bg-background">
-                        <button
-                            id="directors-overview"
-                            className="rounded-t-md py-2 px-4 bg-highlight-blue border-b border-gray-500 text-left lg:text-center"
-                            onClick={() => {
-                                setCurrentTab('directors');
-                                document.getElementById('directors-overview').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('membership-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('meeting-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                            }}
-                        >
-                            Directors Overview
-                        </button>
-                        <button
-                            id="membership-information"
-                            className="rounded-t-md py-2 px-4 text-left lg:text-center"
-                            onClick={() => {
-                                setCurrentTab('membership-information');
-                                document.getElementById('directors-overview').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('membership-information').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('meeting-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                            }}
-                        >
-                            Membership Information
-                        </button>
-                        <button
-                            id="meeting-information"
-                            className="rounded-t-md py-2 px-4 text-left lg:text-center"
-                            onClick={() => {
-                                setCurrentTab('meeting-information');
-                                document.getElementById('directors-overview').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('membership-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
-                                document.getElementById('meeting-information').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
-                            }}
-                        >
-                            General Meetings
-                        </button>
+            <div className="w-full h-full">
+                <div id="content" className="main">
+                    <div id="introduction" className="px-6 pb-5">
+                        <p className="block py-2">
+                            Our commitment to transparency and excellence is reflected in the way we govern our society. Our elected body of students from the
+                            Faculty of Science is dedicated to serving and representing you, the students. Learn more about the structure and leadership
+                            that drives our mission forward.
+                        </p>
                     </div>
-                    <div className="bg-highlight-blue rounded-b-md px-4 py-2">
-                        {currentTab === 'directors' && <Directors />}
-                        {currentTab === 'membership-information' && <Membership />}
-                        {currentTab === 'meeting-information' && <Meeting />}
-                    </div>
-                    <div id='governance-policies'>
+                    <div className="content px-6">
+                        <div className="space-y-2 flex flex-col lg:flex-row lg:space-y-0 text-highlight-dark font-semibold bg-background">
+                            <button
+                                id="directors-overview"
+                                className="rounded-t-md py-2 px-4 bg-highlight-blue border-b border-gray-500 text-left lg:text-center"
+                                onClick={() => {
+                                    setCurrentTab('directors');
+                                    document.getElementById('directors-overview').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('membership-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('meeting-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                }}
+                            >
+                                Directors Overview
+                            </button>
+                            <button
+                                id="membership-information"
+                                className="rounded-t-md py-2 px-4 text-left lg:text-center"
+                                onClick={() => {
+                                    setCurrentTab('membership-information');
+                                    document.getElementById('directors-overview').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('membership-information').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('meeting-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                }}
+                            >
+                                Membership Information
+                            </button>
+                            <button
+                                id="meeting-information"
+                                className="rounded-t-md py-2 px-4 text-left lg:text-center"
+                                onClick={() => {
+                                    setCurrentTab('meeting-information');
+                                    document.getElementById('directors-overview').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('membership-information').classList.remove('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                    document.getElementById('meeting-information').classList.add('bg-highlight-blue', 'border-b', 'border-gray-500');
+                                }}
+                            >
+                                General Meetings
+                            </button>
+                        </div>
+                        <div className="bg-highlight-blue rounded-b-md px-4 py-2">
+                            {currentTab === 'directors' && <Directors />}
+                            {currentTab === 'membership-information' && <Membership />}
+                            {currentTab === 'meeting-information' && <Meeting />}
+                        </div>
+
                         <h2 className="py-2 pt-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">Governance Policies</h2>
-                        <div className="grid grid-cols-1 lg:grid-cols-[70%_30%] items-center justify-start lg:justify-center">
-                            <section className="px-4">
-                                <p>To ensure the highest standards of operation, we adhere to a set of governance policies (<span className="font-bold">add documents and design</span>):</p>
-                                <ul className="px-4 list-none">
-                                    <li>USSTM Constitution</li>
-                                    <li>USSTM By-Laws</li>
-                                    <li>USSTM Financial Policies</li>
-                                    <li>USSTM Election Procedures</li>
-                                </ul>
-                            </section>
+                        <p>To ensure the highest standards of operation, we adhere to a set of governance policies: </p>
+                    </div>
+                </div>
+
+                <div className="bg-highlight-dark my-4 text-background">
+                    <div className="main">
+                        <div className="flex flex-col lg:flex-row lg:justify-between justify-center items-center py-8 mx-6 lg:space-y-0 > * + * space-y-12 > * + * ">
+                            <div className="flex flex-col items-center space-y-4 > * + *">
+                                <GrDocumentText  size={70}/>
+                                <p>USSTM Constitution</p>
+                                <p className="lg:text-sm text-xs italic pt-2 text-stone-300">
+                                Last Updated: January 2024
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center space-y-4 > * + *">
+                                <GrDocumentText  size={70}/>
+                                <p>USSTM By-Laws</p>
+                                <p className="lg:text-sm text-xs italic pt-2 text-stone-300">
+                                Last Updated: January 2024
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center space-y-4 > * + *">
+                                <GrDocumentText  size={70}/>
+                                <p>USSTM Financial Policies</p>
+                                <p className="lg:text-sm text-xs italic pt-2 text-stone-300">
+                                Last Updated: June 2021
+                                </p>
+                            </div>
+                            <div className="flex flex-col items-center space-y-4 > * + *">
+                                <GrDocumentText  size={70}/>
+                                <p>USSTM Election Procedures</p>
+                                <p className="lg:text-sm text-xs italic pt-2 text-stone-300">
+                                Last Updated: February 2023
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="pt-2 pb-6">
+                </div>
+
+                <div className="main">
+                    <div className="pt-2 pb-6 px-6">
                         <h2 className="py-2 pt-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">Your Voice Matters</h2>
-                        <p className="px-4">
+                        <p>
                             As members of our society, your input is invaluable. Reach out to us with event ideas, suggestions for new services, or any
                             criticisms or complaints. You are the watchdogs for USSTM, ensuring we adhere to our Constitution, By-Laws, and other policies. If you have any
                             concerns about our policies, please reach out to our VP Operations at <a href="mailto:vp.operations@usstm.ca">vp.operations@usstm.ca</a>.
