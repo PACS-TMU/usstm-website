c6a61c0306f90a245b72c9221aa70f3f0eeb4ab2
Syed Iltefat <syxddd@proton.me>
2024-01-21 19:53:48 -0500

    Rendered about items to the page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react';
 import Header from "@/app/_components/general/header";
-
+import Link from "next/link";
 
 export default function About() {
     const [ aboutItems, setAboutItems ] = useState( [] );
@@ -25,6 +25,13 @@ export default function About() {
 
     }
 
+    let subItems = [ ];
+
+    aboutItems.map( item => {
+        if ( item.id === 2 ){
+            subItems = item.subItems;
+        }
+    });
 
     
     
@@ -33,17 +40,21 @@ export default function About() {
         <section>
             <Header title={`About`} />
             <div className="main">
-                {
-                    aboutItems.map( ( aboutItem ) => (
-                        <div>
-                            <p> </p> 
+            <div className="container p-4 mx-auto main text-center">
+                <p className="text-xl font-semibold mb-4">Sorry, but we could not find what you were looking for. did you mean one of these?</p>
+                <div className="flex flex-wrap gap-10 mx-auto justify-center">
+                    { 
+                        subItems.map( ( subItem ) => (
+                        <div key={ subItem.id } className="mb-4 flex flex-col bg-gray-100 shadow-md p-4 w-[90%] md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-md">
+                            <Link href={ subItem.path }>
+                                <p className="text-lg font-semibold mb-2 cursor-pointer">{ subItem.itemName }</p>
+                            </Link>
+                            <p className="no-underline text-gray-600">{ subItem.description }</p>
                         </div>
-                    ) 
-                        
-                    )
-
-                }
-            
+                    ))
+                    }
+                </div>
+                </div>           
             </div>
         </section>
     )
