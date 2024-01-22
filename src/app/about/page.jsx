722ec9905df88b0d77d61c8b0869f0c5b73c18b5
Syed Iltefat <syxddd@proton.me>
2024-01-21 19:37:42 -0500

    implemented fetch nav items function

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,49 @@
+'use client'
+import { useState, useEffect } from 'react';
 import Header from "@/app/_components/general/header";
 
+
 export default function About() {
+    const [ aboutItems, setAboutItems ] = useState( [] );
+    
+    useEffect( () => 
+    {
+        fetchNavData();
+    }, [] );
+    
+    const fetchNavData = async ( ) => 
+    {
+        try {
+            const res = await fetch( '/data/nav-items.json' ); 
+            const data = await res.json( );
+            console.log( data );
+            setAboutItems( data );
+
+        } catch ( error ) {
+            console.error( 'Error fetching sub items data:', error );     
+        }
+
+    }
+
+
+    
+    
+    
     return (
         <section>
             <Header title={`About`} />
             <div className="main">
+                {
+                    aboutItems.map( ( aboutItem ) => (
+                        <div>
+                            <p> </p> 
+                        </div>
+                    ) 
+                        
+                    )
+
+                }
+            
             </div>
         </section>
     )
