771f5761bdf491811458e9de1515ed3795b808dc
monakouzi <mo.nakouzi03@gmail.com>
2024-01-29 15:01:50 -0500

    Creating committee component

--- src/app/about/your-team/directors/page.jsx
+++ src/app/about/your-team/directors/page.jsx
@@ -1,10 +1,12 @@
 import Header from "@/app/_components/general/header";
+import Committee from "@/app/about/_components/committee"
 
 export default function Director() {
     return (
         <section>
             <Header title={`Board of Director`} />
             <div className="main">
+                <Committee />
             </div>
         </section>
     )
--- src/app/about/your-team/events-committee/page.jsx
+++ src/app/about/your-team/events-committee/page.jsx
@@ -1,10 +1,12 @@
 import Header from "@/app/_components/general/header";
+import Committee from "@/app/about/_components/committee"
 
 export default function EventsCommittee() {
     return (
         <section>
             <Header title={`Events Committee`} />
             <div className="main">
+                <Committee />
             </div>
         </section>
     )
