44e9196c03a142cc8ecfec7d2d60d00d1fd679f4
monakouzi <mo.nakouzi03@gmail.com>
2024-01-29 16:42:43 -0500

    mapping board members onto website

--- src/app/about/_components/committee.jsx
+++ src/app/about/_components/committee.jsx
@@ -1,5 +1,43 @@
-export default function Committee({ data }) {
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+export default function Committee({ fileName }) {
+    const [members, setMembers] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/data/committees/${fileName}.json`);
+                const data = await response.json();
+                setMembers(data);
+            } catch (error) {
+                console.error('Error fetching governance data: ', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div>Hello HAHAH</div>
+        <div className="grid gap-5 grid-cols-1 mx-12 lg:mx-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-4 items-center justify-center">
+            {members.map((item) => (
+                <div key={item.id} className="w-full h-full flex flex-col">
+                    <Image
+                        src={`/images/about/${fileName}/${item.image}`}
+                        width={1200}
+                        height={800}
+                        className="w-full h-auto"
+                    />
+                    <div>
+                        {item.name}
+                    </div>
+                    <div>
+                        {item.position}
+                    </div>
+                </div>
+            ))
+            }
+        </div>
+
     )
 }
