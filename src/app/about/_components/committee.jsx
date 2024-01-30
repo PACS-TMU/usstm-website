acde5e907432af14d1e146d09deaf9ccebb7725f
monakouzi <mo.nakouzi03@gmail.com>
2024-01-30 18:05:04 -0500

    fix image extensions

--- src/app/about/_components/committee.jsx
+++ src/app/about/_components/committee.jsx
@@ -2,8 +2,29 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
-export default async function Committee({ fileName }) {
+export default function Committee({ fileName }) {
     const [members, setMembers] = useState([]);
+    const blur = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/4" +
+    "gHYSUNDX1BST0ZJTEUAAQEAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAA" +
+    "AAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAA" +
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZX" +
+    "NjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAA" +
+    "BUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAA" +
+    "DxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1" +
+    "AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA" +
+    "9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjA" +
+    "AAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxAD" +
+    "b/2wBDACgcHiMeGSgjISMtKygwPGRBPDc3PHtYXUlkkYCZlo+AjIqgtObDoKrarYqMyP/L2u71" +
+    "////m8H////6/+b9//j/2wBDASstLTw1PHZBQXb4pYyl+Pj4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj" +
+    "4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj4+Pj/wAARCACCAIIDASIAAhEBAxEB/8QAFwAAAwEAAA" +
+    "AAAAAAAAAAAAAAAAECA//EABgQAQEBAQEAAAAAAAAAAAAAAAABERIC/8QAFgEBAQEAAAAAAAAAAA" +
+    "AAAAAAAAEC/8QAFREBAQAAAAAAAAAAAAAAAAAAABH/2gAMAwEAAhEDEQA/ANzh4cjTIioUipEVUVE" +
+    "xUAzIIoACiaVUmiJpVVLFEBWFgiQrACuRjTkYiow8VgwCMAUwQAwRgRKAJwsXgwRGFjTBijPA0wAvk" +
+    "YvBjKs8LGlibFEA6VAgQAwRgZlFQBgw5DwCwYrDwEYF4AMAIFU1dRQTUVVTVCpFS0FHE6cBcVERcBUV" +
+    "CioAACAAAAAAKinai0CrO1VqLVCtLStLVFaqVnqpQaSrjOVcqDSKjOVcoKAlCAAAFpWo6K+gVai0rU2q" +
+    "gtRaLU2gLU6VpaorVSs9VKDWVUrKVcqDWVcrGVUoNZT1nKfQNNDPoCsui6ZdDoRpfSb6R0XSoq1NpWla" +
+    "AtLRpaIenKjT0GkqpWUqpRWsqp6Yz0qegbdH0x6PoVr0GXQBh0OmejVZrTotRp6grRqdAh6QID0aRCK09" +
+    "SNVWkp6z09Faaes9PQXoRoBiABDMAAYCAIAQgAAAChmAKZgCgAA/9k=";
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,9 +49,9 @@ export default async function Committee({ fileName }) {
                         alt={item.name}
                         width={1200}
                         height={800}
-                        loading="lazy"
+                        priority={true}
                         placeholder="blur"
-                        blurDataURL={`data:image;/images/about/${fileName}/${item.image}`}
+                        blurDataURL={blur}
                         className="w-full h-auto aspect-square object-cover object-top shadow-md rounded-full"
                     />
                     <div>
