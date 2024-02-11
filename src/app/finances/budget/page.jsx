8763c878876a7a99321d5cd9a2246bd498f11cba
monakouzi <mo.nakouzi03@gmail.com>
2024-02-10 23:23:30 -0500

    finalizing budgets page, governance and elections page

--- src/app/finances/budget/page.jsx
+++ src/app/finances/budget/page.jsx
@@ -25,14 +25,21 @@ export default function Budget() {
         <section>
             <Header title={`Budget`} />
             <div className="main lg:text-lg">
+                <p className="p-3">
+                    The USSTM budget is a document that outlines the expected income and expenses for the upcoming year and is ratified by the membership 
+                    during our general meetings. Budget requests can be made by student groups or by independent students. For Independent Student Funding, please
+                    visit our <Link href="/finances/conferences-competitions">Conferences & Competitions Page</Link>. Budget details are described in this page.
+                </p>
                 <div className="intro md:w-4/5 lg:w-3/4 mx-auto bg-highlight-blue rounded-sm shadow-md flex flex-col py-8 justify-center items-center">
                     <h3 className="my-2 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
                         Student Levy
                     </h3>
                     <p className="my-3 mx-6 lg:w-1/2 text-center">
-                        The student levy is a fee that is charged to all students at the beginning of each semester. The fee is
-                        used to fund student activities, clubs, and events. The student levy is $30 per semester and is
-                        non-refundable.
+                        The student levy is a fee that is charged to all students at the beginning of each semester. The Levy 
+                        (in addition to money provided by the Dean's Office in the Faculty of Science) is used to fund USSTM and 
+                        its operations, as well as student activities, clubs, and events. The student levy is $30 per semester 
+                        and is non-refundable. It is how we (and the student groups ratified under us) are able to provide the 
+                        services and events that we do!
                     </p>
                     <hr className="border border-gray-400 lg:w-3/4 w-4/5 mt-4 mb-5 mx-auto" />
                     <h3 className="my-2 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
@@ -59,21 +66,56 @@ export default function Budget() {
                     <h2 className="pb-2 pt-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
                         Our Past & Current Budgets
                     </h2>
+                    <p className="my-3">
+                        Below are the budgets that have been approved/ratified by our membership for the current and past years. Click 
+                        on the year to view the budget details.
+                    </p>
                     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 lg:gap-6 pt-4">
                         {budgets.map((budget, index) => (
-                            <div key={index} className="bg-highlight-blue rounded-sm shadow-md p-4">
-                                <h3 className="text-xl lg:text-2xl font-serif font-semibold tracking-wide text-center">
-                                    {budget.title}
-                                </h3>
+                            <div key={index} className="text-center">
+                                <button className="bg-highlight-blue rounded-sm shadow-md w-full h-full p-4 text-xl lg:text-2xl font-serif font-semibold tracking-wide hover:scale-105">
+                                    {budget.title} <br /> Budget
+                                </button>
                             </div>
                         ))}
                     </div>
                 </div>
 
                 <div className="resources my-2 lg:my-6 py-2">
-                    <h2 className="pb-2 pt-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
+                    <h2 className="pt-2 pb-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
                         Resources
                     </h2>
+                    <div className="flex flex-wrap gap-4 mx-auto justify-center">
+                        <div className="bg-highlight-blue rounded-sm shadow-md w-full h-full p-4">
+                            <h2 className="text-xl lg:text-2xl font-serif font-semibold tracking-wide">
+                                Account Codes
+                            </h2>
+                            <p className="m-4">
+                                For the account codes, check out our list of <a href="https://docs.google.com/document/d/1_W61LmlF0CECeZMLu1jCtVsvhq8HVpq5F0A9Ldzi9Jc/view" target="_blank">Account Codes</a> that we use for our accounting. If you are not sure which account your
+                                purchase goes under, please confirm with the VP Finance through email or in person.
+                            </p>
+                        </div>
+                        <div className="bg-highlight-blue rounded-sm shadow-md w-full h-full p-4">
+                            <h2 className="text-xl lg:text-2xl font-serif font-semibold tracking-wide">
+                                Funding Request Committee Public Folder
+                            </h2>
+                            <p className="m-4">
+                                The Funding Request Committee (FRC) Public Folder contains all the documents and resources that are used by the FRC in order to make decisions
+                                on the budget requests. Please click <a href="https://drive.google.com/drive/folders/0B0YoFRsbVn0jZlpQbTBOMVFnbTA?resourcekey=0-sqqYIbHC_gojvK8ZuOAtnw" target="_blank">here</a> to
+                                access the public folder.
+                            </p>
+                        </div>
+                        <div className="bg-highlight-blue rounded-sm shadow-md w-full h-full p-4">
+                            <h2 className="text-xl lg:text-2xl font-serif font-semibold tracking-wide">
+                                How to Read a Budget
+                            </h2>
+                            <p className="m-4">
+                                Budgets are sometimes hard to read and understand. This document will help you understand how to read a budget, which will help you understand
+                                our budgets and help you create your own as well. Please click <a href="#" target="_blank">here</a> to
+                                access the document.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
