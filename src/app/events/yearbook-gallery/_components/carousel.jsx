1f4338af3ad03b169c5e0903b5fadce647b0f685
Andre <dtandre331@gmail.com>
2024-02-16 13:07:24 -0500

    custom arrow fix

--- src/app/events/yearbook-gallery/_components/carousel.jsx
+++ src/app/events/yearbook-gallery/_components/carousel.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import Image from 'next/image';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { CustomNext, CustomPrev } from './CustomArrow';
+import { CustomNext, CustomPrev } from './customarrow';
 
 export default function Carousel() {
     const [yearbookData, setYearbookData] = useState([]);
