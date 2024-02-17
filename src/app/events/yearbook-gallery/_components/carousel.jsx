2219f822e11d890384584b4516de5719d796ec3e
Andre <dtandre331@gmail.com>
2024-02-17 16:20:55 -0500

    utilized swiperjs
    - react-slick -> swiperjs
    - couldn't figure out how to click on the image to go to that image from the current image of the swiper carousel
    - overflow doesn't exist
    - is responsive

--- src/app/events/yearbook-gallery/_components/carousel.jsx
+++ src/app/events/yearbook-gallery/_components/carousel.jsx
@@ -1,10 +1,11 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Slider from 'react-slick';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import Image from 'next/image';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { CustomNext, CustomPrev } from './customarrow';
 
 export default function Carousel() {
     const [yearbookData, setYearbookData] = useState([]);
@@ -21,53 +22,57 @@ export default function Carousel() {
         } catch (err) {
             console.error(err);
         }
-    }
-
-    const settings = {
-        className: "center",
-        focusOnSelect: true,
-        centerMode: true,
-        infinite: true,
-        slidesToShow: 3,
-        speed: 500,
-        nextArrow: <CustomNext />,
-        prevArrow: <CustomPrev />,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
     };
 
     return (
         <div className="slider-container my-[3.5rem] mx-[0.8rem]">
-            <Slider {...settings}>
+            <Swiper
+                modules={[Navigation, Pagination]}
+                slidesPerGroupSkip={0}
+                navigation
+                loop={yearbookData.length > 1 ? true : false}
+                centeredSlides={true}
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true
+                }}
+                breakpoints={
+                    {
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 20
+                        },
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20
+                        },
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 20
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 20
+                        }
+                    } 
+                }
+                className="w-full h-auto text-center text-lg flex justify-center items-center"
+            >
                 {yearbookData.map((image, index) => (
-                    <div key={index} className="cursor-pointer">
-                        <Image src={image.path} alt={image.title} width={1920} height={1080} className="px-[1.25rem]" />
-                        <h1 className="text-center">{image.title}, {image.year}</h1>
-                    </div>
+                    <SwiperSlide key={index}>
+                        <Image
+                            src={image.path}
+                            alt={image.title}
+                            width={5184}
+                            height={3456}
+                            priority={true}
+                            className="rounded-md"
+                        />
+                        
+                        <h1 className="text-center mb-10">{image.title}, {image.year}</h1>
+                    </SwiperSlide>
                 ))}
-            </Slider>
+            </Swiper>
         </div>
     );
 }
