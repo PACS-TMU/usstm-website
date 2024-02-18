8bb25fb0d3e58a8380315203da5082f1f534249f
williamKhine <williamkhine@outlook.com>
2024-02-18 17:30:33 -0500

    added icon and center not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,11 @@
+import {IoIosConstruct} from "react-icons/io";
+import {theme} from "/tailwind.config";
+
 export default function NotFound() {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
+    return (
+        <div className={'flex flex-col h-[calc(100dvh-136px)] justify-center items-center'}>
+            <IoIosConstruct size={100} color={theme.extend.colors["highlight-dark"]}/>
+            <h1 className={'font-serif text-3xl'}>404 | Page Not Found</h1>
+        </div>
+    );
 }
