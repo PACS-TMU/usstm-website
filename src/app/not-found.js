6b6b34499b3b2af7977e293f2ed2a8278c151c59
williamKhine <williamkhine@outlook.com>
2024-02-21 18:24:51 -0500

    not found complete

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,13 @@
-import {IoIosConstruct} from "react-icons/io";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import Link from "next/link";
 import {theme} from "/tailwind.config";
 
 export default function NotFound() {
     return (
         <div className={'flex flex-col h-[calc(100dvh-136px)] justify-center items-center'}>
-            <IoIosConstruct size={100} color={theme.extend.colors["highlight-dark"]}/>
-            <h1 className={'font-serif text-3xl'}>404 | Page Not Found</h1>
+            <FaRegQuestionCircle size={100} color={theme.extend.colors["highlight-dark"]} />
+            <h1 className={'font-serif text-3xl mt-4'}>404 | Page Not Found</h1>
+            <p>Are you lost? Click <Link href={'/'}>here</Link> to go back home.</p>
         </div>
     );
 }
