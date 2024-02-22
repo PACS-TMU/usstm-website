85b2439f8c4f63371d86949f2348baa1c692123a
bensonzhang04 <benson.zhang@torontomu.ca>
2024-02-22 06:31:39 -0500

    gov bod discriptions

--- src/app/about/governance/components/directors.jsx
+++ src/app/about/governance/components/directors.jsx
@@ -12,15 +12,24 @@ export default function directors() {
                     <ul className="list-none">
                         <li className="font-semibold tracking-wide">President:</li>
                         <li className="px-4 pb-3 pt-1">
-                            The President is in charge of overseeing the seven (7) VPs, all together
-                            they make up the executive team
+                        The President oversees the organization's operations, represents the USSTM in 
+                        official capacities, and ensures the board fulfills its responsibilities.
                         </li>
 
                         <li className="font-semibold tracking-wide">Vice President:</li>
-                        <li className="px-4 pb-3 pt-1">[Brief description of each VPs portfolio and their responsibilities]</li>
-
+                        <li className="px-4 pb-3 pt-1">
+                            <u>VP Operations</u>: Manages internal affairs, coordinates events and activities, and supports the President in executing their duties.<br></br>
+                            <u>VP Finance</u>: Responsible for managing the organization's finances, budgeting, and financial planning.<br></br>
+                            <u>VP Communications</u>: The VP Communications handles all internal and external communications, including social media, newsletters, and public relations.<br></br>
+                            <u>VP Events</u>: Plans and executes events and activities for USSTM members, fostering a sense of community and engagement.<br></br>
+                            <u>VP External</u>: External manages external partnerships and relationships, representing USSTM to external organizations and stakeholders.</li>
                         <li className="font-semibold tracking-wide">Directors:</li>
-                        <li className="px-4 pb-3 pt-1">The Directors keep the Executives informed about what students in each program want</li>
+                        <li className="px-4 pb-3 pt-1">
+                            <u>Department Directors</u>: Represent specific academic departments within the Faculty of Science, 
+                        advocating for student interests and organizing department-specific events and initiatives.<br></br>
+                        
+                            <u>First-Year Representatives</u>: Represent the interests of first-year students, providing a voice for new students within the organization.
+                        </li>
 
                         <li className="font-semibold tracking-wide">Non-Voting Members:</li>
                         <li className="px-4 pb-3 pt-1">[Brief overview of the roles of Non-Voting Members]</li>
