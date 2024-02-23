ebe351246996c952211a9e5b6909b0d65818ef83
williamKhine <williamkhine@outlook.com>
2024-02-23 01:30:56 -0500

    fixed FAQ page

--- src/app/about/faq/page.jsx
+++
@@ -1,8 +0,0 @@
-import UnderConstruction from "/src/app/_components/general/under-construction";
-export default function FAQ() {
-      return (
-            <section>
-                <UnderConstruction />
-            </section>
-        )
-}
