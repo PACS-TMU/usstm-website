f04444a81befd4f964de49241e946f7c8431f436
MoNakouzi <mo.nakouzi03@gmail.com>
2024-02-24 14:01:03 -0500

    adding page not found to root of nav items and fixing imports

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -1,10 +1,13 @@
+"use client";
 import Header from "@/app/_components/general/header";
+import InvalidPage from "@/app/_components/general/invalid-page";
 
 export default function Community() {
     return (
         <section>
             <Header title={`Community`} />
             <div className="main">
+                <InvalidPage id={6} />
             </div>
         </section>
     )
