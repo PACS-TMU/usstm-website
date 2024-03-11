2313b2c94913008ca6fb374e5b4eac19c6ad0318
monakouzi <mo.nakouzi03@gmail.com>
2024-03-11 03:59:16 -0400

    adding meta property prompted by vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
-import Parallax from "./_components/parallax"
-import Pillars from "./_components/pillars"
-import Land from "./_components/land"
-import USSTM from "./_components/usstm"
-import FAQ from "./_components/faq"
+import Parallax from "./_components/parallax";
+import Pillars from "./_components/pillars";
+import Land from "./_components/land";
+import USSTM from "./_components/usstm";
+import FAQ from "./_components/faq";
+import Head from "next/head";
 
 export const metadata = {
   title: "Home - USSTM",
-  description: "The Undergraduate Science Society of Toronto Metropolitan homepage. You can find information about our organization, our events, our services, and more, here!"
+  description:
+    "The Undergraduate Science Society of Toronto Metropolitan homepage. You can find information about our organization, our events, our services, and more, here!",
 };
-
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta property="og:url" content="Canonical link preview URL" />
+      </Head>
       <Parallax />
       <USSTM />
       <Pillars />
       <Land />
       <FAQ />
     </>
-  )
+  );
 }
