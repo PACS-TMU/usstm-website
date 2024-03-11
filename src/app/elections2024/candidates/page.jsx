c21e5dc481b082265efdacd939108604e308953c
monakouzi <mo.nakouzi03@gmail.com>
2024-03-11 01:04:09 -0400

    Updating content for elections 2024 page

---
+++ src/app/elections2024/candidates/page.jsx
@@ -0,0 +1,11 @@
+export default function Candidates() {
+    return (
+        <div>
+            <h1>Candidates</h1>
+            <ul>
+                <li>John Doe</li>
+                <li>Jane Doe</li>
+            </ul>
+        </div>
+    );
+}
--- src/app/elections2024/page.jsx
+++ src/app/elections2024/page.jsx
@@ -10,10 +10,10 @@ export default function Elections2024() {
             <div className="main lg:text-lg">
                 <div id="introduction" className="pb-5">
                     <p className="block py-2">
-                        The 2024 elections are coming up and we are excited to announce that nominations are
-                        now open for the 2024-2025 Board! All candidates should watch the info session recording.
-                        Important parts will be highlighted at the all-candidates meeting. If you cannot attend
-                        the All Candidates Meeting, you MUST contact <a href='mailto:cro@usstm.ca' target="_blank">cro@usstm.ca</a>.
+                        The 2024 elections have begun! Nomination period has ended and the campaigning period started.
+                        All eligible candidates must have already attended the All Candidates Meeting or had a proxy attend 
+                        on their behalf. If you have any elections related questions, please contact the Chief Returning Officer
+                        at <a href='mailto:cro@usstm.ca' target="_blank">cro@usstm.ca</a>.
                     </p>
                     <p className="block py-2">
                         Please visit our <Link href="/about/governance">governance page</Link> for more details about
@@ -23,8 +23,24 @@ export default function Elections2024() {
                         LIVE INFO SESSION: Monday 26 Feb 2024, 4PM @ Science Lounge
                     </p>
                     <p className="block py-2">
-                        INFO SESSION RECORDING: Coming Soon...
+                        INFO SESSION SLIDES: <a href='https://docs.google.com/presentation/d/1fqdrmb8lyNT3S_5aE1N81FOySbj8UEi_6e15wPyJypQ/edit?usp=sharing' target="_blank">Click Here!</a>
                     </p>
+                </div>
+
+                <div className="lg:w-3/4 lg:mx-auto bg-highlight-blue rounded-sm shadow-md my-8">
+                    <div className="intro w-full md:w-3/4 mx-auto grid gird-cols-1 md:grid-cols-[30%_30%_40%] justify-center items-center gap-5 py-24">
+                        <h2 className="text-center p-2 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
+                            Campaigning Period - Now Ongoing!
+                        </h2>
+                        <hr className="hidden md:block border border-gray-400 rotate-90 w-full " />
+                        <div className="px-4 md:px-0">
+                            <p className="py-2 xl:text-xl">
+                                The campaigning period has started and will end on March 15th.
+                                Please check our <Link href='/elections2024/candidates'>2024 Candidates Page</Link> to 
+                                see our candidates for the year.
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="schedule py-2">
@@ -43,25 +59,6 @@ export default function Elections2024() {
                     </div>
                 </div>
 
-                <div className="lg:w-3/4 lg:mx-auto bg-highlight-blue rounded-sm shadow-md my-8">
-                    <div className="intro w-full md:w-3/4 mx-auto grid gird-cols-1 md:grid-cols-[30%_30%_40%] justify-center items-center gap-5 py-24">
-                        <h2 className="text-center p-2 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
-                            Nomination Form - Now Open!
-                        </h2>
-                        <hr className="hidden md:block border border-gray-400 rotate-90 w-full " />
-                        <div className="px-4 md:px-0">
-                            <p className="pt-2 text-highlight">
-                                ON A COMPUTER HIGHLY RECOMMENDED
-                            </p>
-                            <p className="py-2 xl:text-xl">
-                                Nomination forms are available starting Feb 22nd. 
-                                Please <a href='https://forms.gle/cxm4yQ4dYefKrUBJ6' target="_blank">click here</a> to 
-                                access the nomination form.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
                 <div className="important-dates py-2">
                     <h2 className="py-4 text-2xl lg:text-3xl xl:text-4xl font-serif font-semibold tracking-wide">
                         Important Dates
@@ -73,7 +70,7 @@ export default function Elections2024() {
                         <li className="py-2">Nomination Period: Feb 22nd to Mar 7th</li>
                         <li className="py-2">Live Info Session: Monday, Feb 26th, 4PM @ Science Lounge</li>
                         <li className="py-2">
-                            All Candidates Meeting: Mar 8th, 4PM @ Science Lounge, eligible candidates - invite only
+                            All Candidates Meeting: Mar 7th, eligible candidates - invite only
                         </li>
                         <li className="py-2">Executive Debate: Mar 11th (as necessary)</li>
                         <li className="py-2">Campaign Period: Mar 11th to 15th</li>
