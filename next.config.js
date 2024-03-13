a6947d3022878b31a48ca1088cec550017464640
MoNakouzi <mo.nakouzi03@gmail.com>
2024-03-13 17:55:28 -0400

    giving up on image randomization

--- next.config.js
+++ next.config.js
@@ -1,28 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/images/elections2024/cro.jpg',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-cache',
-                    },
-                ],
-            },
-            {
-                source: '/images/elections2024/cro-ken.jpg',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-cache',
-                    },
-                ],
-            },
-        ]
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
