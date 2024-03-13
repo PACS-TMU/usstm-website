6707e5e7eb2f5795c773e7a05226750004052dca
MoNakouzi <mo.nakouzi03@gmail.com>
2024-03-13 16:23:35 -0400

    adding Q&A content and meet CRO section

--- src/app/elections2024/candidates/_components/candidate.jsx
+++ src/app/elections2024/candidates/_components/candidate.jsx
@@ -1,5 +1,5 @@
 "use client;"
-import {useState} from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import parse from 'html-react-parser';
 import QandA from './q-and-a';
@@ -8,7 +8,14 @@ export default function Candidate({ index, candidate, list }) {
     const [showQA, setShowQA] = useState(false);
 
     return (
-        <div className="flex flex-col">
+        <div
+            onKeyDown={(e) => {
+                if (e.key === "Escape") {
+                    setShowQA(false);
+                }
+            }}
+            className="flex flex-col"
+        >
             <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
                 <div className="flex flex-col lg:flex-row items-center text-center lg:w-[50%] lg:max-w-[50%] lg:text-left lg:ml-2 lg:mr-4">
                     <div className="grid grid-rows-[80%_20%] grid-cols-1 w-[50%] items-center justify-center">
@@ -38,7 +45,11 @@ export default function Candidate({ index, candidate, list }) {
             </div>
             {list.length > 1 && index !== list.length - 1 && <hr className="w-full py-6" />}
             {showQA && (
-                <QandA candidate={candidate} setShowQA={setShowQA} showQA={showQA} />
+                <QandA
+                    candidate={candidate}
+                    setShowQA={setShowQA}
+                    showQA={showQA}
+                />
             )}
         </div>
     );
