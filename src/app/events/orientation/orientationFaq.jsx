6c62afc5716bf43cb29e0e413b70394a53749340
monakouzi <mo.nakouzi03@gmail.com>
2024-08-12 17:22:35 -0400

    fixing linting errors

--- src/app/events/orientation/orientationFaq.jsx
+++ src/app/events/orientation/orientationFaq.jsx
@@ -5,7 +5,7 @@ export default function OrientationFAQ() {
     return (
         <div className="flex flex-wrap items-center justify-center gap-4">
             {faqData.map((faq) => (
-                <OrientationFAQItem question={faq.question} answer={faq.answer} />
+                <OrientationFAQItem key={faq.id} question={faq.question} answer={faq.answer} />
             ))}
         </div>
     )
