d0fc0ff3c5198d79ae25d3c6b97d8878f066a540
monakouzi <mo.nakouzi03@gmail.com>
2025-02-21 12:49:31 -0500

    Creating the elections 2025 page

--- src/app/_components/general/banner.jsx
+++ src/app/_components/general/banner.jsx
@@ -1,17 +1,18 @@
-'use client';
-import {useState} from "react";
-import {RxCross1} from "react-icons/rx";
+// 'use client';
+// import {useState} from "react";
+// import {RxCross1} from "react-icons/rx";
 import Link from "next/link";
 import './banner.css';
 
 export default function Banner() {
-    const [shown, setShown] = useState(true);
+    // const [shown, setShown] = useState(true);
     const banner = <span>
-            Orientation is around the corner! Check out the details on the <Link href="/events/orientation">orientation page</Link>!
+            The 2025-26 Elections are right around the corner. Click <Link href="/elections2025">here</Link> for more info!
         </span>;
     return (
-        shown && <div className="blink banner shadow-md w-full bg-highlight-blue text-foreground flex items-center justify-center">
-            <h1 className={'p-2 font-serif font-semibold text-center text-sm md:text-lg lg:text-xl xl:text-2xl'}>
+        // shown && 
+        <div className="blink banner shadow-md w-full bg-highlight-blue text-foreground flex items-center justify-center">
+            <h1 className={'p-2 font-serif font-semibold text-center text-sm md:text-lg xl:text-xl'}>
                 {banner}    
             </h1>
             {/* <button className={'border p-0.5'} onClick={() => {setShown(!shown)}}>
