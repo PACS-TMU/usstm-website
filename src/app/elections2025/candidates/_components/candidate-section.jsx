be93a1904a7856eadd2458e737c39e55f26b70f8
Noah Friedman <noah.friedman@torontomu.ca>
2025-03-19 16:48:06 -0400

    Sync `dev` to `main` (#158)
    * Implement release workflow (#145)
    
    * Update build workflow to run on PRs to `dev`
    
    * Update build workflow to run on PRs to `main`
    
    * Release 0.2.2 (#156)
    
    * Updating the candidates page
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Mo Nakouzi <mo.nakouzi03@gmail.com>

--- src/app/elections2025/candidates/_components/candidate-section.jsx
+++ src/app/elections2025/candidates/_components/candidate-section.jsx
@@ -5,31 +5,31 @@ export default function CandidateSection({ role, candidates }) {
     let presidents = [];
     let ops = [];
     let finance = [];
-    let communications = [];
-    let studentlife = [];
+    // let communications = [];
+    // let studentlife = [];
     let academics = [];
     let external = [];
     let equity = [];
-    let bio = [];
+    // let bio = [];
     let biomed = [];
     let cs = [];
-    let math = [];
+    // let math = [];
 
     if (role === "Executives") {
         presidents = candidates[0];
         ops = candidates[1];
         finance = candidates[2];
-        communications = candidates[3];
-        studentlife = candidates[4];
-        academics = candidates[5];
-        external = candidates[6];
-        equity = candidates[7];
+        // communications = candidates[3];
+        // studentlife = candidates[4];
+        academics = candidates[3];
+        external = candidates[4];
+        equity = candidates[5];
     }
     else if (role === "Directors") {
-        bio = candidates[0];
-        biomed = candidates[1];
-        cs = candidates[2];
-        math = candidates[3];
+        //bio = candidates[0];
+        biomed = candidates[0];
+        cs = candidates[1];
+        //math = candidates[3];
     }
     else {
         console.error('Invalid role:', role);
@@ -43,79 +43,103 @@ export default function CandidateSection({ role, candidates }) {
                         President Candidate(s):
                     </p>
                     {presidents.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={presidents} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={presidents} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl  font-semibold">
                         Vice President Operations Candidate(s):
                     </p>
                     {ops.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={ops} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={ops} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl  font-semibold">
                         Vice President Finance Candidate(s):
                     </p>
                     {finance.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={finance} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={finance} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
-                    <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl  font-semibold">
+                    {/* <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl  font-semibold">
                         Vice President Communications Candidate(s):
                     </p>
                     {communications.map((candidate, index) => (
                         <Candidate key={index} index={index} candidate={candidate} list={communications} />
-                    ))}
-                    <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
+                    ))} */}
+                    {/* <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Vice President Student Life Candidate(s):
                     </p>
                     {studentlife.map((candidate, index) => (
                         <Candidate key={index} index={index} candidate={candidate} list={studentlife} />
-                    ))}
+                    ))} */}
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Vice President External Candidate(s):
                     </p>
                     {external.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={external} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={external} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Vice President Academics Candidate(s):
                     </p>
                     {academics.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={academics} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={academics} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Vice President Equity Candidate(s):
                     </p>
                     {equity.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={equity} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={equity} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
                 </div>) :
                 (<div>
-                    <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
+                    {/* <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Biology Director Candidate(s):
                     </p>
                     {bio.map((candidate, index) => (
                         <Candidate key={index} index={index} candidate={candidate} list={bio} />
-                    ))}
+                    ))} */}
 
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Biomedical Sciences Director Candidate(s):
                     </p>
                     {biomed.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={biomed} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={biomed} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
 
                     <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Computer Science Director Candidate(s):
                     </p>
                     {cs.map((candidate, index) => (
-                        <Candidate key={index} index={index} candidate={candidate} list={cs} />
+                        <div key={index}>
+                            <Candidate index={index} candidate={candidate} list={cs} />
+                            <hr className="w-full py-6 mt-6" />
+                        </div>
                     ))}
 
-                    <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
+                    {/* <p className="text-lg lg:mx-2 mb-4 mt-6 lg:text-xl xl:text-2xl font-semibold">
                         Math and Its Applications Director Candidate(s):
                     </p>
                     {math.map((candidate, index) => (
                         <Candidate key={index} index={index} candidate={candidate} list={math} />
-                    ))}
+                    ))} */}
                 </div>
                 )
             }
