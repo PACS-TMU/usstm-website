ee2be2f36f1ba39ea1e4c55776af126585f91ddb
Maruf Ahmed <25273018+maruf99@users.noreply.github.com>
2025-04-03 00:57:05 -0400

    fix: use local path for building committee pages

--- src/app/about/your-team/[committee]/page.jsx
+++ src/app/about/your-team/[committee]/page.jsx
@@ -1,13 +1,11 @@
 import { CommitteePage } from ".";
+import committees from "../../../../../public/data/about/your-team/your-team.json";
 
 export default function GeneratedPage({ params }) {
     return <CommitteePage params={params}/>;
 }
 
-
 export async function generateStaticParams() {
-	const committees = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/about/your-team/your-team.json`).then((res) => res.json());
-
 	return committees.map((committee) => ({
 		committee: committee.path,
 	}));
