f185062f763f342be9f97833ef12b0d2127631e3
Arsal Abrar <81710174+arsal28@users.noreply.github.com>
2025-11-25 21:47:42 -0500

    fix: candidates in wrong roles (#190)
    * fix: candidates in wrong roles
    
    * cleanup: remove unnecessary console log

--- src/app/elections2025/candidates/_components/collapsible.jsx
+++ src/app/elections2025/candidates/_components/collapsible.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState, useRef, useEffect } from "react";
 import Arrow from "@/app/_components/general/arrow";
 import CandidateSection from "./candidate-section";
@@ -6,72 +6,76 @@ import autoAnimate from "@formkit/auto-animate";
 import { useContentItem } from "@/lib/use-content";
 
 export default function Collapsible(props) {
-    const [isVisible, setVisible] = useState(false);
-    const parent = useRef(null);
+  const [isVisible, setVisible] = useState(false);
+  const parent = useRef(null);
 
-    useEffect(() => {
-        parent.current && autoAnimate(parent.current);
-    }, [parent]);
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
 
-    const { data } = useContentItem('community', 'election-candidates');
-    const candidates = data?.content?.candidates || [];
+  const { data } = useContentItem("community", "election-candidates");
+  const candidates = data?.content?.candidates || [];
 
-    const toggle = () => {
-        setVisible(!isVisible);
-    }
+  const toggle = () => {
+    setVisible(!isVisible);
+  };
 
-    let allExecutives = [];
-    let allDirectors = [];
+  let allExecutives = [];
+  let allDirectors = [];
 
-    if (props.sectionName === "Executives") {
-        // const presidents = candidates.filter(candidate => candidate.position === 'President' || candidate.position === 'Co-President');
-        // const ops = candidates.filter(candidate => candidate.position === 'Vice President Operations');
-        // const finance = candidates.filter(candidate => candidate.position === 'Vice President Finance');
-        //const communications = candidates.filter(candidate => candidate.position === 'Vice President Communications');
-        // const studentlife = candidates.filter(candidate => candidate.position === 'Vice President Student Life');
-        const academics = candidates.filter(candidate => candidate.position === 'Vice President Academics');
-        // const external = candidates.filter(candidate => candidate.position === 'Vice President External');
-        // const equity = candidates.filter(candidate => candidate.position === 'Vice President Equity');
-        allExecutives = [academics];
-    }
-    else if (props.sectionName === "Directors") {
-        //const bio = candidates.filter(candidate => candidate.position === 'Biology Director');
-        //const biomed = candidates.filter(candidate => candidate.position === 'Biomedical Science Director');
-        //const cs = candidates.filter(candidate => candidate.position === 'Computer Science Director');
-        //const chem = candidates.filter(candidate => candidate.position === 'Chemistry Director');
-        // const math = candidates.filter(candidate => candidate.position === 'Math and Its Applications Director');
-        allDirectors = [/*bio, biomed, cs, chem*/];
-    }
-    else {
-        console.error('Invalid role:', props.sectionName);
-    }
+  if (props.sectionName === "Executives") {
+    // const presidents = candidates.filter(candidate => candidate.position === 'President' || candidate.position === 'Co-President');
+    // const ops = candidates.filter(candidate => candidate.position === 'Vice President Operations');
+    // const finance = candidates.filter(candidate => candidate.position === 'Vice President Finance');
+    const communications = candidates.filter(
+      (candidate) => candidate.position === "Vice President Communications"
+    );
+    // const studentlife = candidates.filter(candidate => candidate.position === 'Vice President Student Life');
+    const academics = candidates.filter(
+      (candidate) => candidate.position === "Vice President Academics"
+    );
+    // const external = candidates.filter(candidate => candidate.position === 'Vice President External');
+    // const equity = candidates.filter(candidate => candidate.position === 'Vice President Equity');
+    allExecutives = [academics, communications];
+  } else if (props.sectionName === "Directors") {
+    //const bio = candidates.filter(candidate => candidate.position === 'Biology Director');
+    //const biomed = candidates.filter(candidate => candidate.position === 'Biomedical Science Director');
+    //const cs = candidates.filter(candidate => candidate.position === 'Computer Science Director');
+    //const chem = candidates.filter(candidate => candidate.position === 'Chemistry Director');
+    // const math = candidates.filter(candidate => candidate.position === 'Math and Its Applications Director');
+    allDirectors = [
+      /*bio, biomed, cs, chem*/
+    ];
+  } else {
+    console.error("Invalid role:", props.sectionName);
+  }
 
-    return (
-        <div className="py-2" ref={parent}>
-            <button className="faq w-full py-2 sticky" onClick={toggle}>
-                <div className={`question text-left flex justify-between items-center py-2 ${isVisible ? "bg-highlight-blue rounded-sm" : "bg-background"}`}>
-                    <p className="text-xl mx-2 lg:text-2xl xl:text-3xl font-semibold">{props.sectionName}</p>
-                    <div className="mx-2">
-                        <Arrow direction={isVisible && "up"} />
-                    </div>
-                </div>
-            </button>
-            {isVisible &&
-                <div className="answer text-left mx-4 py-2 text-foreground lg:text-lg">
-                    {props.sectionName === "Executives" ?
-                        <CandidateSection
-                            role="Executives"
-                            candidates={allExecutives}
-                        /> :
-                        <CandidateSection
-                            role="Directors"
-                            candidates={allDirectors}
-                        />
-                    }
-                </div>
-            }
-            <hr />
+  return (
+    <div className="py-2" ref={parent}>
+      <button className="faq w-full py-2 sticky" onClick={toggle}>
+        <div
+          className={`question text-left flex justify-between items-center py-2 ${
+            isVisible ? "bg-highlight-blue rounded-sm" : "bg-background"
+          }`}
+        >
+          <p className="text-xl mx-2 lg:text-2xl xl:text-3xl font-semibold">
+            {props.sectionName}
+          </p>
+          <div className="mx-2">
+            <Arrow direction={isVisible && "up"} />
+          </div>
         </div>
-
-    )
+      </button>
+      {isVisible && (
+        <div className="answer text-left mx-4 py-2 text-foreground lg:text-lg">
+          {props.sectionName === "Executives" ? (
+            <CandidateSection role="Executives" candidates={allExecutives} />
+          ) : (
+            <CandidateSection role="Directors" candidates={allDirectors} />
+          )}
+        </div>
+      )}
+      <hr />
+    </div>
+  );
 }
